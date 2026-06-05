0cd28789c3f366ff0b25542b6bf5c0df577df4ec
Lance <lanceharoldmelgarejo@gmail.com>
2026-06-05 14:56:37 +0800

    Add Hello World

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@ void setup() {
     Serial.begin(115200);
     Serial.println("Hello World!");
     Serial.println("Ako itu si Tel");
+     Serial.println("Hello World!");
+ Serial.println("Ako itu si Tel");
 }
 
 void loop() {
